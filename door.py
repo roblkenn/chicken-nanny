2ce1ebbd719c818ea75154c7abf4e7d9835e5151
Robert Kennedy <robert076kennedy@gmail.com>
2021-06-24 21:44:47 -0400

    Added ram sensing support with INA219 current sensor

--- door.py
+++ door.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
-from gpiozero import Motor
+import board
 import click
-from time import sleep
+from adafruit_ina219 import INA219
+from busio import I2C
+from gpiozero import Motor
 
 @click.command()
 @click.option('--open/--close', required=True, default=True, help='Flag to open of close door.')
@@ -11,19 +13,21 @@ def handleDoor(open):
     click.echo("Opening..." if open else "Closing...")
 
     motor = Motor(11, 13, 15, False)
+    sensor = INA219(I2C(board.SCL, board.SDA))
 
     if open:
         motor.forward()
     else:
         motor.backward()
 
-    handleStop(motor)
+    handleStop(motor, sensor)
 
     click.echo("Opened" if open else "Closed")
 
-def handleStop(motor):
-    TIME_TO_RUN = 2
-    sleep(TIME_TO_RUN)
+def handleStop(motor, sensor):
+    # TODO: Test motor ram amperage and adjust value below
+    while sensor.current < 800:
+        continue
     motor.stop()
 
 if __name__ == '__main__':
