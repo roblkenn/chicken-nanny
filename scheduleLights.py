e56e2d5a10cf93e90630e30fec87fa5a1597ea6e
Robert Kennedy <robert076kennedy@gmail.com>
2021-08-28 15:02:01 -0400

    Updated schedule to control lights using cron

---
+++ scheduleLights.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+from datetime import timedelta, timezone
+import datetime
+from suntime import Sun
+from crontab import CronTab
+
+LATITUDE = 41.99
+LONGITUDE = 83.43
+
+sun = Sun(LATITUDE, -LONGITUDE)
+
+# currentDate = datetime.datetime(2021, 12, 22)
+currentDate = datetime.date.today()
+sunriseTime = sun.get_local_sunrise_time(currentDate)
+sunsetTime = sun.get_local_sunset_time(currentDate)
+
+# Workaround for a crappy library
+if sunsetTime < sunriseTime:
+    sunsetTime = sunsetTime + timedelta(days=1)
+
+lightsOffTime = sunriseTime + timedelta(hours=14)
+shouldUseLights = lightsOffTime > sunsetTime
+sunsetOffset = lightsOffTime - sunsetTime
+
+print(f'Sunrise: {sunriseTime}')
+print(f'Sunset: {sunsetTime}')
+print(f'Lights Out: {lightsOffTime}')
+print(f'Should Use Lights: {shouldUseLights}')
+print(f'Offset from Sunset: {sunsetOffset}')
+print(f'Offset formatted for heliocron: {sunsetOffset.seconds//3600:02d}:{(sunsetOffset.seconds//60)%60:02d}')
+
+utc_dt = datetime.datetime.now(timezone.utc)
+currentTime = utc_dt.astimezone()
+print(f'Current Time: {currentTime}')
+
+if shouldUseLights:
+    lightsOnTime = sunsetTime - timedelta(minutes=30)
+    cron = CronTab(user='pi')
+    cron.remove_all('lights.py')
+
+    lightsOnJob = cron.new(command=f'heliocron --latitude {LATITUDE}N --longitude {LONGITUDE}W wait --event sunset --offset -00:30 && /home/pi/chicken-nanny/lights.py --on')
+    lightsOnJob.setall('0 15 * * *')
+
+    lightsOffJob = cron.new(command=f'heliocron --latitude {LATITUDE}N --longitude {LONGITUDE}W wait --event sunset --offset {sunsetOffset.seconds//3600:02d}:{(sunsetOffset.seconds//60)%60:02d} && /home/pi/chicken-nanny/lights.py --off')
+    lightsOffJob.setall('0 15 * * *')
+
+    cron.write()
